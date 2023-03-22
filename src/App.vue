9b739042aa23934ebc423b1308ea6d255f9a2b67
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2023-03-22 11:48:16 -0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,47 @@
 <template>
-  <Navbar />
+  <Navbar :logo="logo_src" :alt="app_name" />
   <router-view />
   <Footer />
 </template>
 
 <script lang="js">
 import Navbar from './components/Navbar.vue';
-import Footer from './views/Footer.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
     Navbar,
     Footer,
+  },
+  data() {
+    return {
+      logo_src: '/img/logo.png',
+      app_name: 'Make Your Burguer',
+    }
   }
 }
 </script>
 
-<style></style>
+<style>
+* {
+  font-family: Helvetica, sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+
+}
+
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu burguer:</h1>
+    <BurguerForm />
   </div>
 </template>
 
 <script>
-
+import Banner from '@/components/Banner.vue';
+import BurguerForm from '@/components/BurguerForm.vue';
 export default {
   name: 'HomeView',
+  components: {
+    Banner,
+    BurguerForm,
+  }
 }
 </script>
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
